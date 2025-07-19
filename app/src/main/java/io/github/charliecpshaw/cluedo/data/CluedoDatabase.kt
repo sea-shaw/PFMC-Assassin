606a4a57f22ca3923f1fb8d7aca0240ced9c62f3
Charlie Shaw <114476631+charliecpshaw@users.noreply.github.com>
2025-07-19 12:14:16 +0100

    add type converters for dates

--- app/src/main/java/io/github/charliecpshaw/cluedo/data/CluedoDatabase.kt
+++ app/src/main/java/io/github/charliecpshaw/cluedo/data/CluedoDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [
@@ -19,6 +20,7 @@ import androidx.room.RoomDatabase
     version = 1,
     exportSchema = false,
 )
+@TypeConverters(Converters::class)
 abstract class CluedoDatabase : RoomDatabase() {
 
     abstract fun gameDao(): GameDao
