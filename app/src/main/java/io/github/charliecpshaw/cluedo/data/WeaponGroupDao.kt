1a4e57df16f3e90363ec0e047fd3e467a2eb08bb
Charlie Shaw <114476631+charliecpshaw@users.noreply.github.com>
2025-07-20 12:05:20 +0100

    add Dao annotation

--- app/src/main/java/io/github/charliecpshaw/cluedo/data/WeaponGroupDao.kt
+++ app/src/main/java/io/github/charliecpshaw/cluedo/data/WeaponGroupDao.kt
@@ -1,5 +1,6 @@
 package io.github.charliecpshaw.cluedo.data
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -7,6 +8,7 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface WeaponGroupDao {
 
     @Query("SELECT * FROM weapon_group ORDER BY name ASC")
