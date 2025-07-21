cc42f738812fbb065a94743a5bd31e828ee55f13
Charlie Shaw <114476631+charliecpshaw@users.noreply.github.com>
2025-07-21 15:53:19 +0100

    change min sdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.charliecpshaw.cluedo"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
