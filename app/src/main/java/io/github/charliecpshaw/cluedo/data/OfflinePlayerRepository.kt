181460e922bf26000dd9612889f9dd5c28665248
Charlie Shaw <114476631+charliecpshaw@users.noreply.github.com>
2025-07-23 11:33:28 +0100

    create player repository skeleton

--- app/src/main/java/io/github/charliecpshaw/cluedo/data/OfflinePlayerRepository.kt
+++ app/src/main/java/io/github/charliecpshaw/cluedo/data/OfflinePlayerRepository.kt
@@ -1,4 +1,37 @@
 package io.github.charliecpshaw.cluedo.data
 
-class OfflinePlayerRepository {
+import kotlinx.coroutines.flow.Flow
+
+class OfflinePlayerRepository(
+    private val playerGroupDao: PlayerGroupDao,
+    private val playerDao: PlayerDao,
+) : PlayerRepository {
+    override fun getPlayerGroupStream(id: Long): Flow<PlayerGroup> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllPlayerGroupsStream(): Flow<List<PlayerGroup>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlayerStream(id: Long): Flow<Player> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllPlayersInGroupStream(groupId: Long): Flow<List<Player>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllActivePlayersInGroupStream(groupId: Long): Flow<List<Player>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertGroup(name: String): Long {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertPlayer(name: String, groupId: Long): Long {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/io/github/charliecpshaw/cluedo/data/PlayerRepository.kt
+++ app/src/main/java/io/github/charliecpshaw/cluedo/data/PlayerRepository.kt
@@ -1,4 +1,19 @@
 package io.github.charliecpshaw.cluedo.data
 
+import kotlinx.coroutines.flow.Flow
+
 interface PlayerRepository {
+    fun getPlayerGroupStream(id: Long): Flow<PlayerGroup>
+
+    fun getAllPlayerGroupsStream(): Flow<List<PlayerGroup>>
+
+    fun getPlayerStream(id: Long): Flow<Player>
+
+    fun getAllPlayersInGroupStream(groupId: Long): Flow<List<Player>>
+
+    fun getAllActivePlayersInGroupStream(groupId: Long): Flow<List<Player>>
+
+    suspend fun insertGroup(name: String): Long
+
+    suspend fun insertPlayer(name: String, groupId: Long): Long
 }
