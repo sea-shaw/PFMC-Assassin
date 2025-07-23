66f90f3dc0f781a9533ec0b80515ee9365102822
Charlie Shaw <114476631+charliecpshaw@users.noreply.github.com>
2025-07-23 14:16:12 +0100

    Update CluedoApp.kt

--- app/src/main/java/io/github/charliecpshaw/cluedo/CluedoApp.kt
+++ app/src/main/java/io/github/charliecpshaw/cluedo/CluedoApp.kt
@@ -2,7 +2,6 @@ package io.github.charliecpshaw.cluedo
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -43,6 +42,6 @@ fun CluedoTopAppBar(
                     )
                 }
             }
-        }
+        },
     )
 }
