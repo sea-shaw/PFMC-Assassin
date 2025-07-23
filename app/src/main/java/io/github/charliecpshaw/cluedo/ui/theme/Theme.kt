326c2847ab704e0282812e4586c689eae8dc92e5
Charlie Shaw <114476631+charliecpshaw@users.noreply.github.com>
2025-07-23 14:16:07 +0100

    add shapes to theme

--- app/src/main/java/io/github/charliecpshaw/cluedo/ui/theme/Theme.kt
+++ app/src/main/java/io/github/charliecpshaw/cluedo/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -53,6 +54,7 @@ fun CluedoTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes,
     )
 }
